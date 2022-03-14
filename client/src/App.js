eb1627de85abcf388510ce69f9404c7470307f4a
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-03-15 00:46:05 +0530

    chat app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import {Box,Stack,VStack,HStack,Input,Center,Button,Spacer,Text} from '@chakra-ui/react'
+import {Box,Stack,VStack,HStack,Input,Center,Button,Spacer,Text,Select} from '@chakra-ui/react'
 import {useState} from 'react';
 import io  from "socket.io-client";
 const ENDPOINT='localhost:2000';
 const  socket= io(ENDPOINT)
 
 function App() {
-const [msg,setmsg]=useState([{mine:false,val:"Ajay"},{mine:true,val:"Vikahs"}]);
+const [msg,setmsg]=useState([]);
 const [val,setval]=useState("");
+const [key,setkey]=useState("");
+const [type,settype]=useState("");
 const text=(e)=>{
    console.log(e.target.value)
    setval(e.target.value);
@@ -15,25 +17,47 @@ const text=(e)=>{
 const Send=()=>{
   if(val==="") return;
  setmsg([...msg,{mine:true,val:val}]); 
- socket.emit("msg",val);
+ let cipher =Encp_Decyp.En(val,key,type)
+ 
+ socket.emit("msg",cipher);
  console.log(val," msg sent ");
  setval("");  
  } 
  
- socket.on("receive",val=>{
-  if(val==="") return;
-  setmsg([...msg,{mine:false,val:val}]); 
+ socket.on("receive",cipher=>{
+  if(cipher==="") return;
+ let plaintext=Encp_Decyp.De(cipher,key,type)
+  setmsg([...msg,{mine:false,val:plaintext}]); 
  })
 
  return (
   <>
-  <VStack w="100%" h="100vh" bg="#D3D3D3">
-  <Center w="90%" h="10vh" bg="white" color="blue" fontSize={40} p={5} > Secure Chat web Site </Center>
-  <Box contenO w="90%" h="80vh" bg="white" color="green" fontSize={20} p={5} > 
+  <VStack w="100%" h="99vh" bg="#D3D3D3">
+  <Center w="90%" h="10vh" bg="white" color="blue" fontSize={40} pl={15} pr={15}  > Secure Chat web Site
+ </Center>
+
+ <HStack w="90%" h="10vh" bg="white" color="blue" pl={15} pr={15}  >
+ 
+     <Select  placeholder='Type '
+       onChange={(e)=>{settype(e.target.value)}}
+      >
+     <option value='t1'>type1</option>
+      <option value='t2'>type2</option>
+     </Select>
+
+
+      <Input placeholder="Enter key "
+              type={type==='t1'?'number':"text"}  
+      onChange={(e)=>{setkey(e.target.value)}}
+       value={key} 
+      />
+  
+ </HStack>
+
+  <Box  w="90%" h="70vh" bg="white" color="green" fontSize={20} pl={15} pr={15} > 
      
      {msg.map(e=>{
-        
-      return  e.mine?(
+     return  e.mine?(
        <HStack>
          <Box maxW="50%" ></Box>
              <Spacer/>
@@ -55,11 +79,15 @@ const Send=()=>{
 
          
      </Box>
-  <HStack>
-   <Input placeholder="hello world " value={val}
+  <HStack w="90%" bg="white" color="green" p={15} >
+   <Input placeholder="Enter text ..." value={val}
       onChange={(e)=>{text(e)}}
-      />  
-   <Button  
+      w="70%"
+      h="150%"
+
+    />  
+      <Spacer/>
+   <Button  w="30%"  h="150%" 
      onClick={e=>Send()}
     >SEND</Button>
   </HStack> 
@@ -70,3 +98,74 @@ const Send=()=>{
 }
 
 export default App;
+
+
+
+
+
+
+
+
+class Encp_Decyp{
+  // TYPE 1 (one) 
+  static t1En(plain,key){
+     let chr=plain.split(""); 
+     let code=[]; chr.forEach(e=>code.push(e.charCodeAt()));
+     let Ecode=[]; code.forEach(e=>Ecode.push(e+parseInt(key)));
+     let cipher=[]; Ecode.forEach(e=>cipher.push(String.fromCharCode(e)));
+     return cipher.join("");
+  }
+  static t1De(cipher,key){
+    let  Ecode=[];
+    cipher.split("").forEach(e=>Ecode.push(e.charCodeAt()))
+    let Dcode=[];Ecode.forEach(e=>Dcode.push(e-parseInt(key)));
+    let plaintext=[]; Dcode.forEach(e=>plaintext.push(String.fromCharCode(e)));
+    return plaintext.join("");
+  }  
+  
+  static t2En(plain,key){
+     let chr=plain.split(""); 
+     let code=[]; chr.forEach(e=>code.push(e.charCodeAt()));
+     let keyCode=[]; key.split("").forEach(e=>keyCode.push(e.charCodeAt()));
+     let keyLen=key.length; 
+    
+     let Ecode=[]; let i=0;
+       code.forEach(e=>{  
+            let x=e+keyCode[parseInt(i++/keyLen)];
+            Ecode.push(String.fromCharCode(x));
+         });
+     return Ecode.join("");
+  }
+  
+  static t2De(cipher,key){
+     let  Ecode=[];
+     cipher.split("").forEach(e=>Ecode.push(e.charCodeAt()))
+     let keyCode=[]; key.split("").forEach(e=>keyCode.push(e.charCodeAt()));
+     let keyLen=key.length; 
+     let Dcode=[]; let i=0;
+     Ecode.forEach(e=>{  
+            let x=e-keyCode[parseInt(i++/keyLen)];
+            Dcode.push(String.fromCharCode(x))
+         });
+     
+        return Dcode.join(""); 
+  }
+  
+  static En(text,key,type){
+     if(type==="t1")
+      return Encp_Decyp.t1En(text,key+0);
+     if(type==="t2")
+     return Encp_Decyp.t2En(text,key+"1234");
+     else 
+      return "error"; "1234"
+   }  
+   static De(text,key,type){
+     if(type==="t1")  
+        return this.t1De(text,key+0);
+     if(type==="t2")  
+       return this.t2De(text,key+"1234");
+     else 
+      return "Error";
+   }
+  }
+  
--- server/index.js
+++ server/index.js
@@ -1,24 +1,17 @@
 const app=require('express')();
 const server=require('http').Server(app);
-
 const io=require('socket.io')(server,{cors:{origin:'*',methods:["GET","POST"]}});
-
 io.on('connection',socket=>{
-
-console.log("new user connected..!",socket.id);
- socket.emit("WlCm","Welcome ...!");
-// This is will be sent for every new socket 
+  console.log("new user connected..!",socket.id);
  
  socket.on("msg",(val=>{
-   console.log(val);
+   console.log("Cipher text ", val);
    socket.broadcast.emit("receive",val);
   }))
 
 // WHEN DISCONNECT
 socket.on('disconnect',()=>
-  {
-   console.log(" User got disconnect !",socket.id);
-  }) 
+{console.log(" User got disconnect !",socket.id);}) 
 
 
 }) 
